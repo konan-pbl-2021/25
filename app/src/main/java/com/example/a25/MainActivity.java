0dcebf20f2feb7d7e64105dfc7db0b1e423f0cbf
Reigenius <reiji0255@gmail.com>
2021-11-12 18:03:14 +0900

    お願いします

--- app/src/main/java/com/example/a25/MainActivity.java
+++ app/src/main/java/com/example/a25/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    GameView gameview = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        gameview = new GameView(this);
+        setContentView(gameview);
     }
 }
