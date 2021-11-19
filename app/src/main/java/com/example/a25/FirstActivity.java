5987d1214ca8edb97ea0a50ff0205593c651f335
1971KAIyoU <s1971020@s.konan-u.ac.jp>
2021-11-19 17:46:50 +0900

    ゲームオーバー画面を作成しました 名前は「GameOverActivity.java」と「activity_game_over.xml」です。 画面遷移ができるか確認するために、少しだけMainActivityとFirstActivityを書き換えましたが、上書きして大丈夫です。

--- app/src/main/java/com/example/a25/FirstActivity.java
+++ app/src/main/java/com/example/a25/FirstActivity.java
@@ -2,7 +2,10 @@ package com.example.a25;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        Button nextButton = (Button)findViewById(R.id.button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, GameOverActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
